5110bc99763bb2c3def5a310c76f3d3cbb655f2b
Bruno Nascimento <brunonascimento0507@gmail.com>
2020-09-24 18:25:09 -0300

    Recuperar Senha

--- index.php
+++ index.php
@@ -149,6 +149,76 @@ $app->post('/admin/users/:iduser', function($iduser) {
    exit;
 
 });
+//forgot
+$app->get('/admin/forgot', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+
+});
+// forgot POST
+$app->post('/admin/forgot', function() {
+
+	$user = User::getForget($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+});
+//Email enviado
+$app->get('/admin/forgot/sent', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+
+});
+//Valida senha
+$app->get('/admin/forgot/reset', function() {
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name" => $user["desperson"],
+		"code" => $_GET["code"],
+	));
+});
+
+//Nova senha
+$app->post('/admin/forgot/reset', function() {
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	User::setFogotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+ 		"cost"=>12
+ 	]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+});
 
 $app->run();
 
---
+++ vendor/brn/php-classes/src/Mailer.php
@@ -0,0 +1,71 @@
+<?php 	
+
+namespace Brn;
+
+use Rain\Tpl;
+
+class Mailer{
+
+	const USERNAME = "brunonascimento0507@gmail.com";
+	const PASSWORD = "";
+	const NAME_FROM = "Patoloja.com";
+
+	private $mail;
+
+	public function __construct($toAddress, $toName, $subject, $tplName, $data = array())
+	{
+
+		$config = array(
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/email/",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"			=> false
+		);
+
+		Tpl::configure( $config );	
+
+		$tpl = new Tpl;
+
+		foreach ($data as $key => $value) {
+			$tpl->assign($key, $value);
+		}
+
+		$html = $tpl->draw($tplName, true);
+
+		$this->mail = new \PHPMailer;
+
+		$this->mail->setLanguage('br');
+
+		$this->mail->isSMTP();
+
+		$this->mail->SMTPDebug = 0;
+
+		$this->mail->Host = 'smtp.gmail.com';
+		
+		$this->mail->Port = 587;
+
+		$this->mail->SMTPSecure = 'tls';
+
+		$this->mail->SMTPAuth = true;
+
+		$this->mail->Username = Mailer::USERNAME;
+
+		$this->mail->Password = Mailer::PASSWORD;
+
+		$this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
+
+		$this->mail->addAddress($toAddress, $toName);
+
+		$this->mail->Subject = $subject;
+
+		$this->mail->msgHTML($html);
+
+		$this->mail->AltBody = 'This is a plain-text message body';
+
+		}
+		function send()
+		{
+			return $this->mail->send();
+		}
+	}
+
+ ?>
